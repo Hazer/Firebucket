a3698549f70786accb9943d257d602bf3cb4d071
Remy Chantenay <remy@bundll.com>
2016-05-23 09:07:33 +0100

    First commit

---
+++ mobile/src/main/java/com/cremy/androidapptemplate/LaunchScreen.java
@@ -0,0 +1,12 @@
+package com.cremy.androidapptemplate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
