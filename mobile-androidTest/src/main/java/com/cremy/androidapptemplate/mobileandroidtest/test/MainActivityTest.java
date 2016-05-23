ba1593c8ce99a21e58e28cba726674e67310bceb
Remy Chantenay <remy.chantenay@carthrtottle.com>
2016-05-23 18:08:48 +0100

    MainActivityTest added

---
+++ mobile-androidTest/src/main/java/com/cremy/androidapptemplate/mobileandroidtest/test/MainActivityTest.java
@@ -0,0 +1,45 @@
+package com.cremy.androidapptemplate.mobileandroidtest.test;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.cremy.androidapptemplate.ui.view.MainActivity;
+import com.cremy.shared.di.app.TestComponentRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by remychantenay on 21/05/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    //region Component and Rule set
+    public final TestComponentRule component =
+            new TestComponentRule(InstrumentationRegistry.getTargetContext());
+    public final ActivityTestRule<MainActivity> main =
+            new ActivityTestRule<>(MainActivity.class, false, false);
+
+    // TestComponentRule needs to go first to make sure the Dagger ShazamAppTestComponent is set
+    // in the Application before any Activity is launched.
+    @Rule
+    public TestRule chain = RuleChain.outerRule(component).around(main);
+    //endregion
+
+    //region Test TextView
+    @Test
+    public void checkTextView() throws InterruptedException {
+        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    }
+    //endregion
+}
